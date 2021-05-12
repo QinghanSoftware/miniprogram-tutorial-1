10450d9992672fcddf10028c2da280fddfc74a5b
Shengda <39946188+shengdamao@users.noreply.github.com>
2021-05-13 00:24:00 +0800

    p37_ 自定义组件_ wxml 中的 Tabs wx:for 列表渲染
    https://www.bilibili.com/video/BV1nE41117BQ?p=37

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,6 +12,31 @@ Component({
    */
   data: {
 
+    tabs:[
+      {
+        id:0,
+        name:"首页",
+        isActive:true
+      },
+      {
+        id:1,
+        name:"原创",
+        isActive:false
+      },
+      {
+        id:2,
+        name:"分类",
+        isActive:false
+      },
+      {
+        id:3,
+        name:"关于",
+        isActive:false
+      },
+
+    ]
+
+
   },
 
   /**
