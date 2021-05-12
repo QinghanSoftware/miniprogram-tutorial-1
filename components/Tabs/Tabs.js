3a3b0a7dc8ae6cd2cbd2eb41126fb0574968ccb3
Shengda <39946188+shengdamao@users.noreply.github.com>
2021-05-12 23:37:36 +0800

    p37_自定义组件 new component, using component
    https://www.bilibili.com/video/BV1nE41117BQ?p=37

---
+++ components/Tabs/Tabs.js
@@ -0,0 +1,23 @@
+// components/Tabs/Tabs.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
