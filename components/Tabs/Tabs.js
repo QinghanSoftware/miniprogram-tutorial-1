93fb3f8d4991cb127c9e46a2f988e030d905638f
Shengda <39946188+shengdamao@users.noreply.github.com>
2021-05-13 01:08:17 +0800

    p38_tap事件 可以 改变isActive 属性
    https://www.bilibili.com/video/BV1nE41117BQ?p=38

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -46,6 +46,23 @@ Component({
 
     handleItemTap(e){
       console.log(e);
+      console.log(e.currentTarget.dataset.index)
+      const index = e.currentTarget.dataset.index
+      // console.log(this.data.tabs[index])
+
+      // let {tabs} = this.data 也可以这样写。 p38教程中是这样的。
+  
+      let tabs = this.data.tabs 
+
+      tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+
+      tabs.forEach(v => { 
+        console.log(v.isActive)        
+      });
+
+      console.log(tabs)
+      console.log(this.data.tabs)
+
     },
 
   }
