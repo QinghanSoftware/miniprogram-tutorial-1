0fe7f7168343e51522f858da9b529d3c70792b22
Shengda <39946188+shengdamao@users.noreply.github.com>
2021-05-14 23:06:02 +0800

    p39_父向子传递数据
    传递Array Tabs 数据。demo17.wxml ---> Tabs.wxml

--- pages/demo17/demo17.js
+++ pages/demo17/demo17.js
@@ -6,6 +6,29 @@ Page({
    */
   data: {
 
+    tabs:[
+      {
+        id:0,
+        name:"首页",
+        isActive:true
+      },
+      {
+        id:1,
+        name:"原创",
+        isActive:false
+      },
+      {
+        id:2,
+        name:"分类",
+        isActive:false
+      },
+      {
+        id:3,
+        name:"关于",
+        isActive:false
+      },
+
+    ]
   },
 
   /**
