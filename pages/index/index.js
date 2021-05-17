40ae0e83fe71e157084dd6113aaa69389271e911
Shengda <39946188+shengdamao@users.noreply.github.com>
2021-05-17 23:48:07 +0800

    p52_swiperData手写 index.js
    https://www.bilibili.com/video/BV1nE41117BQ?p=52

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,45 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    swiperList:[
+      {
+        
+        "image_src":"https://img.alicdn.com/imgextra/i4/2206686532409/O1CN01tDBj7Y1TfMnl48WjC_!!2206686532409-0-lubanimage.jpg",
+        "open_type":"navigate",
+        "goods_id":129,
+        "navigator_url":"/pages/goods_detail/index?goods_id=129",
+      },
+      {
+       
+        "image_src":"https://img.alicdn.com/imgextra/i3/54/O1CN01aMtpWm1CGm4B4Uk5e_!!54-0-luban.jpg",
+        "open_type":"navigate",
+        "goods_id":130,
+        "navigator_url":"/pages/goods_detail/index?goods_id=130",
+      
+
+      },
+      {
+        "image_src":"https://img.alicdn.com/imgextra/i4/86/O1CN01eTZdbK1CVQjvwht7Y_!!86-0-luban.jpg",
+        "open_type":"navigate",
+        "goods_id":131,
+        "navigator_url":"/pages/goods_detail/index?goods_id=131",
+      
+
       }
-    })
+    ]
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
+  
 })
